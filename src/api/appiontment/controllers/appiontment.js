f5885aa42d69f696ae896d888130a16931804fde
younghope1210 <syhope1210@naver.com>
2024-04-13 23:15:29 +0900

    first commit

---
+++ src/api/appiontment/controllers/appiontment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * appiontment controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::appiontment.appiontment');
---
+++ src/api/appiontment/services/appiontment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * appiontment service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::appiontment.appiontment');
